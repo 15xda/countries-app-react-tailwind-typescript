c5f42701d97ccff5e963c3baa9439202ee62cab9
Jacky <adovudzoda@gmail.com>
2025-08-06 18:09:26 +0500

    Last Changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Container from "./Container";
+
 export default function Header() {
   return (
-    <div className="flex bg-[#1F1F1F] items-center h-15 px-10 text-white">
-      <h1>Country Search App</h1>
+    <div className="flex bg-[#1F1F1F] h-15 px-10 text-white">
+      <Container className="flex text-[20px] items-center w-full">
+        <h1>Countries App</h1>
+      </Container>
     </div>
   );
 }
